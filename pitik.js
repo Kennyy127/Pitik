1a000a18e756000f4f2e37961e48f3c1fee6a148
Kennyy127 <kennedy21budiman@gmail.com>
2025-03-06 16:37:21 +0700

    Add files via upload

---
+++ pitik.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("contact").addEventListener("click", function () {
+        alert("Hubungi kami di nomor yang tersedia.");
+    });
+});
